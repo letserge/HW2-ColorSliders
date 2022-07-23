da93b9007ca1d2367dbb7f7a3e74e813f0f0c7da
Ranenburger Pro <usethchance@gmail.com>
2022-07-23 09:31:21 +0300

    Initial Commit

---
+++ HW2 ColorSliders/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2 ColorSliders
+//
+//  Created by Tomas Anderson on 23.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
